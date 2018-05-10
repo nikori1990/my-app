6a0ee4faaf8a9ede02e07317e4bcebb3fe4770d4
元炳烨 <nikori1990@gmail.com>
2018-05-10 23:43:01 +0800

    init

---
+++ app/model/DataXY.js
@@ -0,0 +1,12 @@
+Ext.define('MyApp.model.DataXY', {
+    extend: 'MyApp.model.Base',
+
+    fields: [
+        {
+            name: 'xvalue'
+        },
+        {
+            name: 'yvalue'
+        }
+    ]
+});
---
+++ classic/overrides/container/Container.js
@@ -0,0 +1,12 @@
+Ext.define('MyApp.override.container.Container', {
+    override: 'Ext.container.Container',
+ 
+    getDefaultFocus: function () {
+        // defaultFocus on mobile devices causes the virtual keyboard to
+        // immediately display without action so only focus for desktops
+        if (Ext.os.deviceType === 'Desktop') {
+            return this.callParent();
+        }
+    }
+ });
+ 
